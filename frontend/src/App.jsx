601c8d8605d4b9f86e17c7602f4a046bd8acc544
Your Name <your@email.com>
2026-04-29 00:07:58 +0100

    force admin page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Admin from "./pages/Admin";
-import Login from "./pages/Login";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <Admin />;
 }
 
 export default App;

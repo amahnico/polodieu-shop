012a1ecb68ced34356eb5125dcdcfac859a00022
Your Name <your@email.com>
2026-04-29 01:42:17 +0100

    enable shop page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Shop from "./pages/Shop";
 import Admin from "./pages/Admin";
 
 function App() {
-  return <Admin />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Shop />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
